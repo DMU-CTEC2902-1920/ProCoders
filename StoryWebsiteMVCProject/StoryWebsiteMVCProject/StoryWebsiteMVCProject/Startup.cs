03b8ad03f32ebb0e7d73c1d8a81f1a1665a9bc9a
Kopil Kaiser <kopil_kaiser@hotmail.com>
2020-02-14 13:33:05 +0000

    Created new MVCProject for our project StoryWebsite

---
+++ StoryWebsiteMVCProject/StoryWebsiteMVCProject/StoryWebsiteMVCProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StoryWebsiteMVCProject.Startup))]
+namespace StoryWebsiteMVCProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
