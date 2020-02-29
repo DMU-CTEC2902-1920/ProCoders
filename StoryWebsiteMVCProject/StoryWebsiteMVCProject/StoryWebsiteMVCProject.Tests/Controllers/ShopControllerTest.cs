3f476a6a5bfa0436cd4eb56104628192fef26c64
Kopil Kaiser <kopil_kaiser@hotmail.com>
2020-02-29 08:22:48 +0000

    Completed Lab work for Week 21 - Controllers. Topic tags: Dependency Injection (loosely couple the Model to the Controller)

---
+++ StoryWebsiteMVCProject/StoryWebsiteMVCProject/StoryWebsiteMVCProject.Tests/Controllers/ShopControllerTest.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using StoryWebsiteMVCProject.Controllers;
+using StoryWebsiteMVCProject.Models;
+
+namespace StoryWebsiteMVCProject.Tests.Controllers
+{
+    [TestClass]
+    public class ShopControllerTest
+    {
+        [TestMethod]
+        public void IndexWelcomeMessage()           
+        {
+            //Arrange
+
+            ShopController controller = new ShopController();
+
+            //Act
+            ViewResult result = controller.Index();
+
+            //Assert
+            Assert.AreEqual("Welcome to the Shop. Kindly spend all your money!", controller.ViewBag.WelcomeMessage);
+
+        }
+
+        [TestMethod]
+        public void ProductName()
+        {
+            //Arrange
+            ProductModel model = new ProductModel();
+            model.Name = "New Product";
+
+            ShopController controller = new ShopController(model);
+
+            //Act
+            ViewResult viewResult = controller.Product();
+            ProductModel result = viewResult.Model as ProductModel;
+
+            //Assert
+            Assert.AreEqual("New Product", result.Name);
+        }
+
+        [TestMethod]
+        public void ProductIsBling()
+        {
+            //Arrange
+            ProductModel model = new ProductModel();
+           
+            model.Name = "Test Product 2";
+            model.Description = "Description of Test Product 2";
+            model.Price = 222.99m;
+
+            ShopController controller = new ShopController(model);
+
+            //Act
+            ViewResult result = controller.Product();
+
+            //Assert
+            Assert.AreEqual("Bling!" , result.ViewBag.SubTitle);
+
+        }
+
+        [TestMethod]
+        public void ProductIsCheapAndNasty()
+        {
+            //Arrange
+
+            ProductModel model = new ProductModel();
+            model.Name = "Test Product1";
+            model.Description = "Description of Test Product 1";
+            model.Price = 2.99m;
+
+            //Inject the controller's dependency on the model so we can test it
+            ShopController controller = new ShopController(model);
+
+            //Act
+            ViewResult result = controller.Product();
+
+            //Assert
+            Assert.AreEqual("Cheap and nasty product!", result.ViewBag.SubTitle);
+
+        }
+    }
+}
---
+++ StoryWebsiteMVCProject/StoryWebsiteMVCProject/StoryWebsiteMVCProject/Controllers/ShopController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StoryWebsiteMVCProject.Models;
+
+namespace StoryWebsiteMVCProject.Controllers
+{
+    
+    public class ShopController : Controller
+    {
+        private ProductModel _model;
+        public ShopController() { }
+        public ShopController(ProductModel model)
+        {
+            _model = model;
+        }
+
+        // GET: Shop
+        public ViewResult Index()
+        {
+            ViewBag.Title = "The Shop";
+            ViewBag.WelcomeMessage = "Welcome to the Shop. Kindly spend all your money!";
+            ViewData["CurrentTime"] = DateTime.Now;
+            return View();
+        }
+
+        public ViewResult Product()
+        {
+            if (_model == null)
+            {
+                _model = new ProductModel();
+                _model.Name = "IPhone 6";
+                _model.Description = "Latest IPhone with things that make it better.";
+                _model.Price = 350.00m;
+            }
+
+            ViewBag.Title = "Product: IPhone 6";
+
+            if (_model.Price > 200)
+            {
+                ViewBag.SubTitle = "Bling!";
+            }
+
+            else
+            {
+                ViewBag.SubTitle = "Cheap and nasty product!";
+            }
+
+            return View(_model);
+        }
+
+        public string Product1(string Type, string Colour)
+        {
+            return String.Format("Product Type = {0}, Colour = {1}", Type, Colour);
+        }
+
+        public string Product2(int id)
+        {
+            return "Product : Id = " + id.ToString();
+        }
+
+        public string Product3()
+        {
+            return "Shop product";
+        }
+
+        public RedirectResult DVD()
+        {
+            return RedirectPermanent("~/shop/product1?type=shirt&colour=blue");
+            //return Redirect("~/Shop/Product1?type=22&colour=44");
+        }
+    }
+}
