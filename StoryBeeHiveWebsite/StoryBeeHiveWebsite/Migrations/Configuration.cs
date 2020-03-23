33d0f5c241b2e008ffa8f6988e0bb06638162b5c
Kopil Kaiser <kopil_kaiser@hotmail.com>
2020-03-23 02:45:06 +0000

    Developed Create Stories page and added new migration seed data

--- StoryBeeHiveWebsite/StoryBeeHiveWebsite/Migrations/Configuration.cs
+++ StoryBeeHiveWebsite/StoryBeeHiveWebsite/Migrations/Configuration.cs
@@ -4,6 +4,8 @@ namespace StoryBeeHiveWebsite.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using System.Collections.Generic;
+    using StoryBeeHiveWebsite.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<StoryBeeHiveWebsite.Models.StoryContext>
     {
@@ -19,6 +21,25 @@ namespace StoryBeeHiveWebsite.Migrations
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
             //  to avoid creating duplicate seed data.
+            var authors = new List<Author>
+            {
+                new Author{AuthorId=1, FirstName="Jakeer", LastName="Gulati"},
+                new Author{AuthorId=2, FirstName="Hugo", LastName="Austin"},
+                new Author{AuthorId=3, FirstName="Qin", LastName="Juang"}
+            };
+
+            authors.ForEach(author => context.Authors.AddOrUpdate(p => p.AuthorId, author));
+            context.SaveChanges();
+
+            var categories = new List<Category>
+            {
+                new Category{CategoryId=1, Name="Horror"},
+                new Category{CategoryId=2, Name="Sci-Fi"},
+                new Category{CategoryId=3, Name="Comedy"}
+            };
+
+            categories.ForEach(category => context.Categories.AddOrUpdate(p => p.CategoryId, category));
+            context.SaveChanges();
         }
     }
 }
